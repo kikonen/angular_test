2a37e04e7778f9a2c9ddb95ddf9e4aaca2f1b37c
kari <mr.kari.ikonen@gmail.com>
2014-04-09 20:33:32 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AngularTest::Application.config.secret_token = '22775d0a025c59d260dd1644794dcb86574f5d0c4eed345d760c9b8e0cc27214688517914dd4913c5288b2d0e85e1ae39b11ce6ee67b5a0ae275f00a104f88aa'
