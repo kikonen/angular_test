a995aede4021d447fb550684a8a5d8d4421b4986
kari <mr.kari.ikonen@gmail.com>
2014-05-05 19:34:10 +0300

    Setup X-CSRF-Token on login

--- app/controllers/api/login_controller.rb
+++ app/controllers/api/login_controller.rb
@@ -2,6 +2,8 @@ module Api
   class LoginController < ApiController
     def create
       cookies[:auth_token] = 'xxx123'
+      # @see http://stackoverflow.com/questions/9362910/rails-warning-cant-verify-csrf-token-authenticity-for-json-devise-requests
+      response.headers['X-CSRF-Token'] = form_authenticity_token
       render json: {}, status: :ok
     end
 
