dba453e261c5bb13993d8176df9821d77443bd55
kari <mr.kari.ikonen@gmail.com>
2014-05-07 23:01:41 +0300

    upload fixes - FIX: must render HEAD

--- app/controllers/api/upload_controller.rb
+++ app/controllers/api/upload_controller.rb
@@ -3,6 +3,7 @@ module Api
     def get_upload
       ap get: params
       logger.info "upload"
+      head :ok
     end
 
     def post_upload
@@ -12,11 +13,13 @@ module Api
       target_file = File.join(Rails.root, "tmp", "foo.txt")
       FileUtils.cp(file, target_file)
       ap sz: File.size(target_file), name: target_file.to_s
+      head :ok
     end
 
     def put_upload
       ap put: params
       logger.info "upload"
+      head :ok
     end
   end
 end
